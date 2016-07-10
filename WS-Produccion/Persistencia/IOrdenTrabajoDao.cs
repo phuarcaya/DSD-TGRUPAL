baa5daa4da17c42f4b8d9a71a8eaa308a99fd048
hugatecar <hugartecar@gmail.com>
2016-07-10 16:25:59 -0500

    Se construye el DAO de Orden de Trabajo

---
+++ WS-Produccion/Persistencia/IOrdenTrabajoDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WS_Produccion.Persistencia
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IOrdenTrabajoDao" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IOrdenTrabajoDao
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ WS-Produccion/Persistencia/OrdenesDao.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WS_Produccion.Persistencia
+{
+    public class OrdenesDao
+    {
+
+        private string cadenaConexion = "Data Source=(local);Initial Catalog=BD_Proyectos;Integrated Security=SSPI";
+        public OrdenTrabajo Crear(OrdenTrabajo ordenTrabajoaAcrear)
+        {
+            OrdenTrabajo ordenTrabajoCreado = null;
+            string sql = "INSERT INTO OrdenTrabajo VALUES (@id, @fecha, @fecharegistro, @fechamodificacion, @Activo, @idestado)";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("@id", ordenTrabajoaAcrear.Id));
+                    comando.Parameters.Add(new SqlParameter("@fecha", ordenTrabajoaAcrear.Fecha));
+                    comando.Parameters.Add(new SqlParameter("@fecharegistro", ordenTrabajoaAcrear.FechaRegistro));
+                    comando.Parameters.Add(new SqlParameter("@fechamodificacion", ordenTrabajoaAcrear.FechaModificacion));
+                    comando.Parameters.Add(new SqlParameter("@Activo", ordenTrabajoaAcrear.Activo));
+                    comando.Parameters.Add(new SqlParameter("@idestado", ordenTrabajoaAcrear.IdEstado));
+                    comando.ExecuteNonQuery();
+                }
+            }
+            ordenTrabajoCreado = Obtener(ordenTrabajoaAcrear.Id);
+            return ordenTrabajoCreado;
+        }
+
+        public OrdenTrabajo Obtener(int id)
+        {
+            OrdenTrabajo OrdenTrabajoEncontrado = null;
+            string sql = "SELECT * FROM OrdenTrabajo WHERE id=@id";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("@id", id));
+                    using (SqlDataReader resultado = comando.ExecuteReader())
+                    {
+                        if (resultado.Read())
+                        {
+                            OrdenTrabajoEncontrado = new OrdenTrabajo()
+                            {
+                                Id = (int)resultado["Id"],
+                                Fecha = (DateTime)resultado["Fecha"],
+                                FechaRegistro = (DateTime)resultado["FechaRegistro"],
+                                FechaModificacion = (DateTime)resultado["FechaModificacion"],
+                                Activo = (bool)resultado["Activo"],
+                                IdEstado = (int)resultado["IdEstado"],
+                            };
+                        }
+                    }
+
+                }
+
+            }
+
+            return OrdenTrabajoEncontrado;
+        }
+        public OrdenTrabajo Modificar(OrdenTrabajo ordenTrabajoaAmodificar)
+        {
+
+            OrdenTrabajo ordenTrabajoModificado = null;
+            string sql = "UPDATE OrdenTrabajo SET anl_nombre=@nombre, anl_seniority=@seniority WHERE anl_id=@id";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("@id", ordenTrabajoaAmodificar.Id));
+                    comando.Parameters.Add(new SqlParameter("@fecha", ordenTrabajoaAmodificar.Fecha));
+                    comando.Parameters.Add(new SqlParameter("@fecharegistro", ordenTrabajoaAmodificar.FechaRegistro));
+                    comando.Parameters.Add(new SqlParameter("@fechamodificacion", ordenTrabajoaAmodificar.FechaModificacion));
+                    comando.Parameters.Add(new SqlParameter("@Activo", ordenTrabajoaAmodificar.Activo));
+                    comando.Parameters.Add(new SqlParameter("@idestado", ordenTrabajoaAmodificar.IdEstado));
+
+
+                    comando.ExecuteNonQuery();
+
+                }
+
+            }
+            ordenTrabajoModificado = Obtener(ordenTrabajoaAmodificar.Id);
+            return ordenTrabajoModificado;
+
+        }
+
+        public void Eliminar(string id)
+        {
+            string sql = "DELETE FROM  OrdenTrabajo WHERE anl_id=@id";
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(sql, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("@id", id));
+                    comando.ExecuteNonQuery();
+
+
+                }
+            }
+        }
+
+    }
+}
