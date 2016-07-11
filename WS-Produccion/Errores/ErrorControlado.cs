7850c81e29190bafe0d70a0ade16abc14647a474
phuarcaya <poncianohc@gmail.com>
2016-07-11 00:01:04 -0500

    Clase control de errores

---
+++ WS-Produccion/Errores/ErrorControlado.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+namespace WS_Produccion.Errores
+{
+    /// <summary>
+    /// <Proposito>Clase que gestiona los errores controlados</Proposito>
+    /// <Detalle>Ejemplo: Devolver mensaje de codigo repetido, stock insuficiente, etc</Detalle>
+    /// </summary>
+    [DataContract]
+    public class ErrorControlado
+    {
+        [DataMember]
+        public string Codigo { get; set; }
+
+        [DataMember]
+        public string Descripcion { get; set; }
+    }
+}
