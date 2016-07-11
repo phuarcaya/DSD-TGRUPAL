d33dd777ea9259aabdee2fb785589beb8a32507e
phuarcaya <poncianohc@gmail.com>
2016-07-11 00:04:53 -0500

    g

---
+++ WS-Produccion/Servicios/ParametroDetalles.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WS_Produccion.Servicios
+{
+    public class ParametroDetalles : IParametroDetalles
+    {
+        public List<ParametroDetalle> ListarParametroDetalle(int idPadre)
+        {
+            return new ParametroDetalleDao().Listar(idPadre);
+        }
+    }
+}
