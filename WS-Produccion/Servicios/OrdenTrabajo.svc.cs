ffc943d128e00cbe080bae73d3f455bf546fced8
hugatecar <hugartecar@gmail.com>
2016-07-13 14:10:17 -0500

    Logica servicio terminada ordenes. !Validar dos errores

--- WS-Produccion/Servicios/OrdenTrabajo.svc.cs
+++ WS-Produccion/Servicios/OrdenTrabajo.svc.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WS_Produccion.Excepciones;
+using WS_Produccion.Persistencia;
 
 namespace WS_Produccion.Servicios
 {
@@ -11,8 +13,38 @@ namespace WS_Produccion.Servicios
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione OrdenTrabajo.svc o OrdenTrabajo.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class OrdenTrabajo : IOrdenTrabajo
     {
-        public void DoWork()
+        private OrdenesDao ordenesDAO = new OrdenesDao();
+
+        public OrdenTrabajo CrearOrdenTrabajo(OrdenTrabajo OrdenTraabajoACrear)
         {
+            if (ordenesDAO.Obtener(OrdenTraabajoACrear.id) != null) //ya existe
+            {
+                throw new FaultException<RepetidosExceptions>(
+                    new RepetidosExceptions()
+                    {
+                        codigo = "101",
+                        descripcion = "La orden de trabajo ya existe"
+                    }, new FaultReason("Error al intentar creacion"));
+
+            }
+            return ordenesDAO.Crear(OrdenTraabajoACrear);
+        }
+
+    
+
+        public void EliminarOrdenTrabajo(int id)
+        {
+            ordenesDAO.Eliminar(id);
+        }
+
+        public OrdenTrabajo ModificarOrdenTrabajo(OrdenTrabajo ordenTrabajoaAmodificar)
+        {
+            return ordenesDAO.Modificar(ordenTrabajoaAmodificar);
+        }
+
+        public OrdenTrabajo ObtenerOrdenTrabajo(int id)
+        {
+            return ordenesDAO.Obtener(id);
         }
     }
 }
