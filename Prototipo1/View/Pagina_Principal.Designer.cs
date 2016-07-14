a9556c9a9be3ae240d6adaef76723731d2289aa1
labarandiaran <labarandiaran@gmail.com>
2016-07-13 20:34:13 -0500

    Estandarizacion de controles de entrada

--- Prototipo1/View/Pagina_Principal.Designer.cs
+++ Prototipo1/View/Pagina_Principal.Designer.cs
@@ -42,9 +42,9 @@
             this.toolStrip3 = new System.Windows.Forms.ToolStrip();
             this.btnClientes = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.btnCronograma = new System.Windows.Forms.ToolStripButton();
-            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.btnTramites = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnCronograma = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.btnRus = new System.Windows.Forms.ToolStripButton();
             this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
@@ -59,6 +59,7 @@
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.Teal;
             this.menuStrip1.BackgroundImage = global::Prototipo1.Properties.Resources.header;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ordenDeCompraToolStripMenuItem,
             this.almacenToolStripMenuItem,
@@ -67,8 +68,8 @@
             this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 20, 0, 20);
-            this.menuStrip1.Size = new System.Drawing.Size(1184, 65);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 25, 0, 25);
+            this.menuStrip1.Size = new System.Drawing.Size(1579, 82);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -79,20 +80,20 @@
             this.aprobarOTToolStripMenuItem});
             this.ordenDeCompraToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ordenDeCompraToolStripMenuItem.Name = "ordenDeCompraToolStripMenuItem";
-            this.ordenDeCompraToolStripMenuItem.Size = new System.Drawing.Size(142, 25);
+            this.ordenDeCompraToolStripMenuItem.Size = new System.Drawing.Size(175, 32);
             this.ordenDeCompraToolStripMenuItem.Text = "Orden de Trabajo";
             // 
             // generarOTToolStripMenuItem
             // 
             this.generarOTToolStripMenuItem.Name = "generarOTToolStripMenuItem";
-            this.generarOTToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.generarOTToolStripMenuItem.Size = new System.Drawing.Size(192, 32);
             this.generarOTToolStripMenuItem.Text = "Generar OT";
             this.generarOTToolStripMenuItem.Click += new System.EventHandler(this.generarOTToolStripMenuItem_Click);
             // 
             // aprobarOTToolStripMenuItem
             // 
             this.aprobarOTToolStripMenuItem.Name = "aprobarOTToolStripMenuItem";
-            this.aprobarOTToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.aprobarOTToolStripMenuItem.Size = new System.Drawing.Size(192, 32);
             this.aprobarOTToolStripMenuItem.Text = "Aprobar OT";
             this.aprobarOTToolStripMenuItem.Click += new System.EventHandler(this.aprobarOTToolStripMenuItem_Click);
             // 
@@ -103,21 +104,21 @@
             this.salidaDeMAteriaPrimaToolStripMenuItem});
             this.almacenToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.almacenToolStripMenuItem.Name = "almacenToolStripMenuItem";
-            this.almacenToolStripMenuItem.Size = new System.Drawing.Size(82, 25);
+            this.almacenToolStripMenuItem.Size = new System.Drawing.Size(99, 32);
             this.almacenToolStripMenuItem.Text = "Almacen";
             // 
             // ingresarProductosTerminadosToolStripMenuItem
             // 
             this.ingresarProductosTerminadosToolStripMenuItem.Name = "ingresarProductosTerminadosToolStripMenuItem";
-            this.ingresarProductosTerminadosToolStripMenuItem.Size = new System.Drawing.Size(294, 26);
-            this.ingresarProductosTerminadosToolStripMenuItem.Text = "Ingresar Productos terminados";
+            this.ingresarProductosTerminadosToolStripMenuItem.Size = new System.Drawing.Size(381, 32);
+            this.ingresarProductosTerminadosToolStripMenuItem.Text = "Ingreso de Productos Terminados";
             this.ingresarProductosTerminadosToolStripMenuItem.Click += new System.EventHandler(this.ingresarProductosTerminadosToolStripMenuItem_Click);
             // 
             // salidaDeMAteriaPrimaToolStripMenuItem
             // 
             this.salidaDeMAteriaPrimaToolStripMenuItem.Name = "salidaDeMAteriaPrimaToolStripMenuItem";
-            this.salidaDeMAteriaPrimaToolStripMenuItem.Size = new System.Drawing.Size(294, 26);
-            this.salidaDeMAteriaPrimaToolStripMenuItem.Text = "Salida de MAteria Prima";
+            this.salidaDeMAteriaPrimaToolStripMenuItem.Size = new System.Drawing.Size(381, 32);
+            this.salidaDeMAteriaPrimaToolStripMenuItem.Text = "Salida de Materia Prima";
             this.salidaDeMAteriaPrimaToolStripMenuItem.Click += new System.EventHandler(this.salidaDeMAteriaPrimaToolStripMenuItem_Click);
             // 
             // acercaToolStripMenuItem
@@ -126,15 +127,15 @@
             this.reporteEficienciaToolStripMenuItem});
             this.acercaToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.acercaToolStripMenuItem.Name = "acercaToolStripMenuItem";
-            this.acercaToolStripMenuItem.Size = new System.Drawing.Size(77, 25);
+            this.acercaToolStripMenuItem.Size = new System.Drawing.Size(93, 32);
             this.acercaToolStripMenuItem.Text = "Reporte";
             this.acercaToolStripMenuItem.Click += new System.EventHandler(this.acercaToolStripMenuItem_Click);
             // 
             // reporteEficienciaToolStripMenuItem
             // 
             this.reporteEficienciaToolStripMenuItem.Name = "reporteEficienciaToolStripMenuItem";
-            this.reporteEficienciaToolStripMenuItem.Size = new System.Drawing.Size(199, 26);
-            this.reporteEficienciaToolStripMenuItem.Text = "reporte eficiencia";
+            this.reporteEficienciaToolStripMenuItem.Size = new System.Drawing.Size(271, 32);
+            this.reporteEficienciaToolStripMenuItem.Text = "Reporte de Eficiencia";
             this.reporteEficienciaToolStripMenuItem.Click += new System.EventHandler(this.reporteEficienciaToolStripMenuItem_Click);
             // 
             // acercaToolStripMenuItem1
@@ -143,13 +144,13 @@
             this.grupoNeptunoToolStripMenuItem});
             this.acercaToolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.acercaToolStripMenuItem1.Name = "acercaToolStripMenuItem1";
-            this.acercaToolStripMenuItem1.Size = new System.Drawing.Size(66, 25);
+            this.acercaToolStripMenuItem1.Size = new System.Drawing.Size(81, 32);
             this.acercaToolStripMenuItem1.Text = "Acerca";
             // 
             // grupoNeptunoToolStripMenuItem
             // 
             this.grupoNeptunoToolStripMenuItem.Name = "grupoNeptunoToolStripMenuItem";
-            this.grupoNeptunoToolStripMenuItem.Size = new System.Drawing.Size(163, 24);
+            this.grupoNeptunoToolStripMenuItem.Size = new System.Drawing.Size(200, 30);
             this.grupoNeptunoToolStripMenuItem.Text = "Grupo Urano";
             this.grupoNeptunoToolStripMenuItem.Click += new System.EventHandler(this.grupoNeptunoToolStripMenuItem_Click);
             // 
@@ -172,10 +173,10 @@
             this.btnEspacial,
             this.toolStripSeparator5,
             this.btnCerrar});
-            this.toolStrip3.Location = new System.Drawing.Point(0, 65);
+            this.toolStrip3.Location = new System.Drawing.Point(0, 82);
             this.toolStrip3.Name = "toolStrip3";
             this.toolStrip3.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip3.Size = new System.Drawing.Size(235, 596);
+            this.toolStrip3.Size = new System.Drawing.Size(308, 732);
             this.toolStrip3.Stretch = true;
             this.toolStrip3.TabIndex = 34;
             // 
@@ -187,32 +188,15 @@
             this.btnClientes.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
             this.btnClientes.Name = "btnClientes";
             this.btnClientes.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnClientes.Size = new System.Drawing.Size(232, 26);
-            this.btnClientes.Text = "&Orden de Trabajo";
+            this.btnClientes.Size = new System.Drawing.Size(305, 30);
+            this.btnClientes.Text = "&Generar Orden de Trabajo";
+            this.btnClientes.ToolTipText = "Generar OT";
             this.btnClientes.Click += new System.EventHandler(this.btnClientes_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(232, 6);
-            // 
-            // btnCronograma
-            // 
-            this.btnCronograma.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCronograma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
-            this.btnCronograma.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnCronograma.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
-            this.btnCronograma.Name = "btnCronograma";
-            this.btnCronograma.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnCronograma.Size = new System.Drawing.Size(232, 26);
-            this.btnCronograma.Text = "&Salida de Materia Prima";
-            this.btnCronograma.ToolTipText = "Cronograma de Vencimientos";
-            this.btnCronograma.Click += new System.EventHandler(this.btnCronograma_Click);
-            // 
-            // toolStripSeparator3
-            // 
-            this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(232, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(305, 6);
             // 
             // btnTramites
             // 
@@ -222,15 +206,33 @@
             this.btnTramites.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
             this.btnTramites.Name = "btnTramites";
             this.btnTramites.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnTramites.Size = new System.Drawing.Size(232, 26);
-            this.btnTramites.Text = "&Aprobación Orden de Trabajo";
-            this.btnTramites.ToolTipText = "Trámite Documentario";
+            this.btnTramites.Size = new System.Drawing.Size(305, 30);
+            this.btnTramites.Text = "&Aprobar Orden de Trabajo";
+            this.btnTramites.ToolTipText = "Aprobar OT";
             this.btnTramites.Click += new System.EventHandler(this.btnTramites_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(305, 6);
+            // 
+            // btnCronograma
+            // 
+            this.btnCronograma.Font = new System.Drawing.Font("Arial Narrow", 12.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCronograma.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCronograma.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCronograma.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
+            this.btnCronograma.Name = "btnCronograma";
+            this.btnCronograma.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnCronograma.Size = new System.Drawing.Size(305, 30);
+            this.btnCronograma.Text = "&Salida de Materias Prima";
+            this.btnCronograma.ToolTipText = "Salida de Materia Prima";
+            this.btnCronograma.Click += new System.EventHandler(this.btnCronograma_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(232, 6);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(305, 6);
             // 
             // btnRus
             // 
@@ -240,15 +242,15 @@
             this.btnRus.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
             this.btnRus.Name = "btnRus";
             this.btnRus.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnRus.Size = new System.Drawing.Size(232, 26);
-            this.btnRus.Text = "&Ingreso de Producto Terminado";
-            this.btnRus.ToolTipText = "Movimientos RUS";
+            this.btnRus.Size = new System.Drawing.Size(305, 30);
+            this.btnRus.Text = "&Ingreso de Productos Terminados";
+            this.btnRus.ToolTipText = "Ingreso de Productos Terminados";
             this.btnRus.Click += new System.EventHandler(this.btnRus_Click);
             // 
             // toolStripSeparator4
             // 
             this.toolStripSeparator4.Name = "toolStripSeparator4";
-            this.toolStripSeparator4.Size = new System.Drawing.Size(232, 6);
+            this.toolStripSeparator4.Size = new System.Drawing.Size(305, 6);
             // 
             // btnEspacial
             // 
@@ -258,15 +260,15 @@
             this.btnEspacial.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
             this.btnEspacial.Name = "btnEspacial";
             this.btnEspacial.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnEspacial.Size = new System.Drawing.Size(232, 26);
-            this.btnEspacial.Text = "&Eficiencia de Producción";
-            this.btnEspacial.ToolTipText = "Movimientos Especial / General";
+            this.btnEspacial.Size = new System.Drawing.Size(305, 30);
+            this.btnEspacial.Text = "&Reporte de Eficiencia";
+            this.btnEspacial.ToolTipText = "Reporte de Eficiencia";
             this.btnEspacial.Click += new System.EventHandler(this.btnEspacial_Click);
             // 
             // toolStripSeparator5
             // 
             this.toolStripSeparator5.Name = "toolStripSeparator5";
-            this.toolStripSeparator5.Size = new System.Drawing.Size(232, 6);
+            this.toolStripSeparator5.Size = new System.Drawing.Size(305, 6);
             // 
             // btnCerrar
             // 
@@ -277,19 +279,20 @@
             this.btnCerrar.Margin = new System.Windows.Forms.Padding(0, 1, 0, 1);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnCerrar.Size = new System.Drawing.Size(232, 26);
+            this.btnCerrar.Size = new System.Drawing.Size(305, 30);
             this.btnCerrar.Text = "&Cerrar Sesión";
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // Pagina_Principal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.ClientSize = new System.Drawing.Size(1579, 814);
             this.Controls.Add(this.toolStrip3);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Pagina_Principal";
             this.Text = "Sistema Producción - Empresa [Muebles Lot\'s S.A.C]";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
