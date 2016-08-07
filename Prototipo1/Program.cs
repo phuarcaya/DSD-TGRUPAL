8456a901ea8ac1f062d51d8791f8e5e4b0bd9481
labarandiaran <labarandiaran@gmail.com>
2016-08-06 23:34:51 -0500

    Optimizaciones de librerias y codigo

--- Prototipo1/Program.cs
+++ Prototipo1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using Prototipo1.View;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Prototipo1
--- WS-Produccion/Interfaces/IArticulos.cs
+++ WS-Produccion/Interfaces/IArticulos.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+﻿using System.ServiceModel;
 using WS_Produccion.Excepciones;
 
 namespace WS_Produccion.Servicios
 {
 
-        [ServiceContract]
+    [ServiceContract]
     public interface IArticulos
     {
         [FaultContract(typeof(SinStockExceptions))]
